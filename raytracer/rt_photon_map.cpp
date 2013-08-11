728d836a17d1f9ed7fa026f35dc664cf5e250337
Daniel Stokes <kupomail@gmail.com>
2013-08-11 08:56:40 -0700

    Some uncommited changes from the independent study

--- raytracer/rt_photon_map.cpp
+++ raytracer/rt_photon_map.cpp
@@ -9,16 +9,9 @@
 #include "data/scene.h"
 #include "Eigen/Dense"
 
-//#define ALPHA 0.918
-//#define BETA 1.953
-//#define PI 3.141592653589793
-//#define E 2.718281828459045
-//#define K 5
+#define FILTER_GAUSS
 
-const int X = 0;
-const int Y = 1;
-const int Z = 2;
-
+const float K = 1.0;
 const float ALPHA = 0.918;
 const float BETA = 1.953;
 const float PI = 3.141592653589793;
@@ -33,49 +26,47 @@ public:
 	Eigen::Vector3f position;
 	Eigen::Vector3f energy;
 	Eigen::Vector3f direction;
+	float distance2;
 	short flag;
-};
-
-class KDNode
-{
-public:
-	Photon* data;
-	class KDNode* left, *right;
 };
 
 void PhotonMap::emit_photon(Scene* scene, Ray* r, Eigen::Vector3f energy, float max_dist, int pass)
 {
 	Result result;
 
-//	if (pass > 1) return;
+	if (pass > 1) return;
 
 	scene->mesh_structure->intersect(r, &result);
 	if (!result.hit) return;
 
-	float dist = 0;
-//	float dist = (result.position - *(r->getOrigin())).norm();
-//	if (dist > max_dist) return;
+//	float dist = 0;
+	float dist = (result.position - *(r->getOrigin())).norm();
+	if (dist > max_dist) return;
 //	energy *=  (max_dist - dist) / max_dist;
 
-//	if (((float)rand())/RAND_MAX < 0.8) {
+	float absorb = (pass == 1) ? 1.0 : 0.6;
+
+	if (((float)rand())/RAND_MAX < absorb) {
 		Photon* p = new Photon();
 		p->position = result.position;
-		p->energy = energy;
+		p->energy = energy;// * (1-diff_ref);
 		p->direction = *r->getDirection();
 		this->photons.push_back(p);
-//	}
-//	else {
+	}
+	else {
 		r->setOrigin(result.position(0), result.position(1), result.position(2));
-		Eigen::Vector3f dir;
-		dir(0) = rand() / rmax_2 - 1;
-		dir(1) = rand() / rmax_2 - 1;
-		dir(2) = rand() / rmax_2 - 1;
-		if (dir.dot(dir) > 1) return;
+		Eigen::Vector3f dir = Eigen::Vector3f(1, 1, 1);
+		while (dir.dot(dir) > 1 || dir.dot(result.normal) < 0) {
+			dir(0) = rand() / rmax_2 - 1;
+			dir(1) = rand() / rmax_2 - 1;
+			dir(2) = rand() / rmax_2 - 1;
+		}
 		r->setDirection(dir(0), dir(1), dir(2));
-		energy = result.material->diffuse_color * energy.norm();
-		energy *= 0.0075;
-//		this->emit_photon(scene, r, energy, max_dist-dist, pass++);
-//	}
+		energy = result.material->diffuse_color * (energy.norm()/255);
+//		std::cout << energy.norm()/255 <<std::endl;
+		//energy *= diff_ref;
+		this->emit_photon(scene, r, energy, max_dist-dist, pass++);
+	}
 
 }
 
@@ -94,49 +85,22 @@ static bool sort_z(Photon* a, Photon* b)
 	return a->position[2] < b->position[2];
 }
 
-KDNode* PhotonMap::balance_photons(int begin, int end, int level)
-{
-	KDNode* node = new KDNode;
-	if (end - begin == 1) {
-		node->data = this->photons[begin];
-		return node;
-	}
-
-	int axis = level % 3;
-
-	if (axis == X)
-		std::sort(this->photons.begin()+begin, this->photons.begin()+end, sort_x);
-	else if (axis == Y)
-		std::sort(this->photons.begin()+begin, this->photons.begin()+end, sort_y);
-	else if (axis == Z)
-		std::sort(this->photons.begin()+begin, this->photons.begin()+end, sort_z);
-
-	Photon* median = this->photons[this->photons.size()/2];
-	median->flag = axis;
-
-	int mid = (begin+end)/2;
-	this->balance_photons(begin, mid, level++);
-	this->balance_photons(mid, end, level++);
-
-	return NULL;
-}
-
 void PhotonMap::generate(Scene* scene, int count)
 {
 	Eigen::Vector3f dir;
 	Eigen::Vector3f energy;
 	Ray ray;
 	Light* light;
-	float max_dist = 20;
+	float max_dist = 12;
 	float rmax_2 = RAND_MAX/2.0;
 	srand(time(NULL));
 	for (int i = 0; i < scene->lights.size(); ++i) {
 		light = scene->lights[i];
-		energy = light->color;
+		energy = 5*light->color/count;
 		for (int j = 0; j < count;) {
 			dir(0) = rand() / rmax_2 - 1;
 			dir(1) = rand() / rmax_2 - 1;
-			dir(2) = rand() / rmax_2 - 1;
+			dir(2) = -rand() / (float)RAND_MAX;
 			
 			if (dir.dot(dir) > 1) continue;
 			j++;
@@ -150,13 +114,15 @@ void PhotonMap::generate(Scene* scene, int count)
 		}
 	}
 
-	float scale = 1.0 / this->photons.size();
+//	float scale = 1.0 / this->photons.size();
+	std::cout << this->photons.size() << std::endl;
+	Eigen::Vector3f esum = Eigen::Vector3f(0, 0, 0);
 	for (int i = 0; i < this->photons.size(); ++i)
 	{
-		this->photons[i]->energy *= scale;
+		esum += this->photons[i]->energy;
+//		this->photons[i]->energy *= scale;
 	}
-
-	this->balance_photons(0, this->photons.size(), 0);
+	std::cout << "Total energy: " << esum << std::endl;
 }
 
 Eigen::Vector3f PhotonMap::lookup(Eigen::Vector3f position, float radius)
@@ -177,7 +143,12 @@ Eigen::Vector3f PhotonMap::lookup(Eigen::Vector3f position, float radius)
 	return result;
 }
 
-Eigen::Vector3f PhotonMap::radiance_estimate(Eigen::Vector3f position, Eigen::Vector3f ray_dir, Eigen::Vector3f normal, float radius)
+static bool _photon_sort(Photon*a, Photon*b)
+{
+	return a->distance2 < b->distance2;
+}
+
+Eigen::Vector3f PhotonMap::radiance_estimate(Eigen::Vector3f position, Eigen::Vector3f ray_dir, Eigen::Vector3f normal, int count, float radius)
 {
 	float r2 = radius * radius;
 	float l2, weight;
@@ -186,40 +157,54 @@ Eigen::Vector3f PhotonMap::radiance_estimate(Eigen::Vector3f position, Eigen::Ve
 	std::vector<Photon*> nearest_photons;
 	Eigen::Vector3f result = Eigen::Vector3f(0, 0, 0);
 	Eigen::Vector3f difference;
-	return result;
+
 	for (int i = 0; i < this->photons.size(); ++i) {
 		p = this->photons[i];
 
 		difference = p->position - position;
 		l2 = difference.dot(difference);
+		p->distance2 = l2;
 		if (l2 < r2) {
 			nearest_photons.push_back(p);
 			if (l2 > max_dist2) max_dist2 = l2;
 		}
 	}
 
-//	float max_dist = sqrt(max_dist2);
-	for (int i = 0; i < nearest_photons.size(); ++i) {
+//	std::cout << (float)nearest_photons.size()/count << std::endl;
+	std::sort(nearest_photons.begin(), nearest_photons.end(), _photon_sort);
+
+#ifdef FILTER_CONE
+	float max_dist = std::sqrt(max_dist2);
+#endif
+
+	for (int i = 0; i < nearest_photons.size() && i < count; ++i) {
 		p = nearest_photons[i];
 
 		difference = p->position - position;
 		l2 = difference.dot(difference);
 
+#ifdef FILTER_GAUSS
 		weight = 1 - pow(E, -BETA * (l2/(2*max_dist2)));
 		weight *= GAUSS_DENOM;
 		weight = ALPHA * (1- weight);
-
-//		difference = p->position - position;
-//		weight = 1 - difference.norm()/(K*max_dist);
-		float lambert = normal.dot(-p->direction);
-		if (lambert < 0.1) lambert = 0.1;
-		if (lambert > 0.9) lambert = 0.9;
-
-		result += p->energy * weight;
+#elif defined(FILTER_CONE)
+		weight = 1 - difference.norm()/(K*max_dist);
+#else
+		weight = 1;
+#endif
+		float brdf = normal.dot(-p->direction);
+		if (brdf > 1.0) brdf = 1.0;
+		if (brdf < 0.0) brdf = 0.0;
+		result += brdf * p->energy * weight;
 	}
 
-//	result = result/((1-(2/3*K))*PI*max_dist2);
-//	result = result/(2*PI*max_dist2);
+#ifdef FILTER_GUASS
+	result = result;
+#elif defined(FILTER_CONE)
+	result = result/((1-(2/3*K))*PI*max_dist2);
+#else
+	result = result/(2*PI*max_dist2);
+#endif
 
 	return result;
 }
--- raytracer/rt_photon_map.h
+++ raytracer/rt_photon_map.h
@@ -9,13 +9,11 @@ class PhotonMap
 {
 private:
 	void emit_photon(class Scene* scene, class Ray* r, Eigen::Vector3f energy, float max_dist, int pass);
-	class KDNode* balance_photons(int begin, int end, int level);
 	std::vector<class Photon*> photons;
-	class KDNode* kdtree;
 public:
 	void generate(class Scene* scene, int count);
 	Eigen::Vector3f lookup(Eigen::Vector3f position, float radius);
-	Eigen::Vector3f radiance_estimate(Eigen::Vector3f position, Eigen::Vector3f ray_dir, Eigen::Vector3f normal, float radius);
+	Eigen::Vector3f radiance_estimate(Eigen::Vector3f position, Eigen::Vector3f ray_dir, Eigen::Vector3f normal, int count, float radius);
 };
 
 #endif
