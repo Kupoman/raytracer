99cca5173978433eb7b96ad587db42c9e70c1161
Daniel Stokes <kupomail@gmail.com>
2013-10-15 13:25:27 -0700

    Forgot a file for the last commit.

--- data/scene.cpp
+++ data/scene.cpp
@@ -39,7 +39,7 @@ void Scene::addMesh(Mesh *mesh)
 
 void Scene::draw(unsigned char *output)
 {
-	int count;
+	int count, material_count;
 	Eigen::Vector3f *positions, *normals;
 	Result *results;
 	ResultOffset *result_offsets;
@@ -49,6 +49,10 @@ void Scene::draw(unsigned char *output)
 	this->rasterizer->drawLights(this->lights);
 	this->rasterizer->drawMeshes();
 	this->rasterizer->getRayTraceData(&count, &positions, &normals);
-	this->raytracer->processRays(*this->camera, count, positions, normals, &results, &result_offsets, &count);
-	this->rasterizer->drawRayData(results, result_offsets, count);
+	this->raytracer->processRays(*this->camera, count, positions, normals, &results, &result_offsets, &count, &material_count);
+	this->rasterizer->drawRayData(results, result_offsets, count, material_count);
+//	unsigned char *color = new unsigned char[4 * this->camera->getWidth() * this->camera->getHeight()];
+//	this->raytracer->renderScene(*this, this->camera->getWidth(), this->camera->getHeight(), color);
+//	this->rasterizer->displayImageData(color);
+//	delete [] color;
 }
